b69333e61f322aa4fc8206e9562f73708bf3418e
aycanogut <aycanonfire@gmail.com>
2021-05-30 23:00:27 +0300

    first conditions is functionable on playRound func

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,7 +1,7 @@
 // DOM declares
 
-// badges
-const badgeYou = document.querySelector('span.is-primary');
+// player badges
+const badgePlayer = document.querySelector('span.is-primary');
 const badgeComputer = document.querySelector('span.is-success');
 
 // turn result
@@ -10,91 +10,77 @@ const turnResult = document.querySelector('.turn-result');
 // progress bar
 const progressBar = document.querySelector('progress');
 
-// player selection of round
+// buttons
+const buttons = document.querySelectorAll('.nes-btn');
 
-// game function
-const checkClick = (e) => {
-  //player's selection data from buttons
-  var playerSelection;
-  if (e.value === 'rock') {
-    playerSelection = 'rock';
-  } else if (e.value === 'paper') {
-    playerSelection = 'paper';
-  } else if (e.value === 'scissors') {
-    playerSelection = 'scissors';
-  }
+// catch button value
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.value)));
 
-  // computer's selection random
-  const computerPlay = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-      return "paper";
-    } else if (randomNumber === 1) {
-      return "paper";
-    } else if (randomNumber === 2) {
-      return "scissors";
-    }
+// computer's random selection
+const computerPlay = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return 'rock';
+  } else if (randomNumber === 1) {
+    return 'paper'
+  } else if (randomNumber === 2) {
+    return "scissors";
   }
-  computerPlay()
+  return randomNumber;
 }
 
+const computerSelection = computerPlay();
 
+const playRound = (playerSelection) => {
 
+  // score counter stuff
+  let playerScore = 0;
+  let computerScore = 0;
+  let playCount = 0;
 
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+    turnResult.innerHTML = 'paper beats rock, AI won this round!';
+    computerScore++;
+    badgeComputer.innerHTML = `${computerScore}`
+    playCount++;
 
-// Rock, Paper veya Scissors butonlarindan birine bastigimda oyun 1 el oynanacak ve cikacak sonuca gore Lets play kismi ve VS kismindaki sonuc badgeleri guncellenecek
-// progress guncellenecek
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    turnResult.innerHTML = 'paper beats rock, you won this round!';
+    playerScore++
+    badgePlayer.innerHTML = `${playerScore}`;
+    playCount++;
 
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    turnResult.innerHTML = 'scissors beats paper, you won this round!';
+    playerScore++;
+    badgePlayer.innerHTML = `${playerScore}`;
+    playCount++;
+    return 'scissors beats paper';
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log('scissors beats paper');
+    computerScore++;
+    playCount++;
+    return 'scissors beats paper';
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log('rock beats scissors');
+    playerScore++;
+    playCount++;
+    return 'rock beats scissors';
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log('rock beats scissors');
+    computerScore++;
+    playCount++;
+    return 'rock beats scissors';
+  } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
+    console.log('tie');
+    return 'tie';
+  }
+}
 
+playRound()
 
-// const playRound = (playerSelection, computerSelection) => {
-//   var playerScore = 0;
-//   var computerScore = 0;
-//   var playCount = 0;
-
-//   if (playerSelection === 'rock' && computerSelection === 'paper') {
-//     console.log('paper beats rock')
-//     computerScore++;
-//     playCount++
-//     return 'paper beats rock';
-//   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-//     console.log('paper beats rock')
-//     playerScore++;
-//     playCount++
-
-//     return 'paper beats rock';
-//   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-//     console.log('scissors beats paper');
-//     playerScore++;
-//     playCount++
-//     return 'scissors beats paper';
-//   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-//     console.log('scissors beats paper');
-//     computerScore++;
-//     playCount++
-//     return 'scissors beats paper';
-//   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-//     console.log('rock beats scissors');
-//     playerScore++;
-//     playCount++
-//     return 'rock beats scissors';
-//   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-//     console.log('rock beats scissors');
-//     computerScore++;
-//     playCount++
-//     return 'rock beats scissors';
-//   } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
-//     console.log('tie');
-//     return 'tie';
-//   }
-// }
 
 // // const game = (playRound) => {
 // //   const playerSelection = prompt('choose').toLowerCase();
 // //   const computerSelection = computerPlay();
 // // }
-
-
-
-
-// playRound(playerSelection, computerSelection)
