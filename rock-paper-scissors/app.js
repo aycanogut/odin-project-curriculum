b391f579340740bea7719a806b95bc46c777c565
aycanogut <aycanonfire@gmail.com>
2021-05-30 16:43:32 +0300

    score badges added

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -9,24 +9,63 @@ const computerPlay = () => {
   }
 }
 
-const playerSelection = prompt('choose').toLowerCase();
+// const playerSelection = prompt('choose').toLowerCase();
 const computerSelection = computerPlay();
 
+
 const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
+  var playerScore = 0;
+  var computerScore = 0;
+
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
     console.log('paper beats rock')
+    computerScore++;
     return 'paper beats rock';
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors') {
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    console.log('paper beats rock')
+    playerScore++;
+    return 'paper beats rock';
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     console.log('scissors beats paper');
+    playerScore++;
     return 'scissors beats paper';
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock') {
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log('scissors beats paper');
+    computerScore++;
+    return 'scissors beats paper';
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     console.log('rock beats scissors');
+    playerScore++;
+    return 'rock beats scissors';
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log('rock beats scissors');
+    computerScore++;
     return 'rock beats scissors';
   } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
     console.log('tie');
     return 'tie';
   }
 }
+
+const game = (playRound) => {
+
+}
+
+// const playRound = (playerSelection, computerSelection) => {
+//   if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
+//     console.log('paper beats rock')
+//     return 'paper beats rock';
+//   } else if (playerSelection === 'scissors' && computerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors') {
+//     console.log('scissors beats paper');
+//     return 'scissors beats paper';
+//   } else if (playerSelection === 'rock' && computerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock') {
+//     console.log('rock beats scissors');
+//     return 'rock beats scissors';
+//   } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
+//     console.log('tie');
+//     return 'tie';
+//   }
+// }
 
 
 playRound(playerSelection, computerSelection)
