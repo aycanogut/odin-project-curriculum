77f6fca0f09d2f8dc6728e2c42e100262588f701
aycanogut <aycanonfire@gmail.com>
2021-05-30 17:34:42 +0300

    checkClick function is functional

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -7,14 +7,16 @@ const badgeComputer = document.querySelector('span.is-success');
 // turn result
 const turnResult = document.querySelector('.turn-result');
 
-// game buttons
-const rockButton = document.querySelector('button.is-error');
-const paperButton = document.querySelector('button.is-success');
-const scissorsButton = document.querySelector('button.is-warning');
+// player selection of round
+let playerSelection;
+
+// take data from buttons
+const checkClick = (clicked_id) => {
+  console.log(clicked_id);
+}
 
 // progress bar
 const progressBar = document.querySelector('progress');
-console.log(progressBar);
 
 const computerPlay = () => {
   const randomNumber = Math.floor(Math.random() * 3);
@@ -27,37 +29,47 @@ const computerPlay = () => {
   }
 }
 
-// const playerSelection = prompt('choose').toLowerCase();
-const computerSelection = computerPlay();
+// Rock, Paper veya Scissors butonlarindan birine bastigimda oyun 1 el oynanacak ve cikacak sonuca gore Lets play kismi ve VS kismindaki sonuc badgeleri guncellenecek
+// progress guncellenecek
+
+
 
 
 const playRound = (playerSelection, computerSelection) => {
   var playerScore = 0;
   var computerScore = 0;
+  var playCount = 0;
 
   if (playerSelection === 'rock' && computerSelection === 'paper') {
     console.log('paper beats rock')
     computerScore++;
+    playCount++
     return 'paper beats rock';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     console.log('paper beats rock')
     playerScore++;
+    playCount++
+
     return 'paper beats rock';
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     console.log('scissors beats paper');
     playerScore++;
+    playCount++
     return 'scissors beats paper';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     console.log('scissors beats paper');
     computerScore++;
+    playCount++
     return 'scissors beats paper';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     console.log('rock beats scissors');
     playerScore++;
+    playCount++
     return 'rock beats scissors';
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     console.log('rock beats scissors');
     computerScore++;
+    playCount++
     return 'rock beats scissors';
   } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
     console.log('tie');
@@ -65,25 +77,12 @@ const playRound = (playerSelection, computerSelection) => {
   }
 }
 
-const game = (playRound) => {
-
-}
-
-// const playRound = (playerSelection, computerSelection) => {
-//   if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
-//     console.log('paper beats rock')
-//     return 'paper beats rock';
-//   } else if (playerSelection === 'scissors' && computerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors') {
-//     console.log('scissors beats paper');
-//     return 'scissors beats paper';
-//   } else if (playerSelection === 'rock' && computerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock') {
-//     console.log('rock beats scissors');
-//     return 'rock beats scissors';
-//   } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
-//     console.log('tie');
-//     return 'tie';
-//   }
+// const game = (playRound) => {
+//   const playerSelection = prompt('choose').toLowerCase();
+//   const computerSelection = computerPlay();
 // }
+
+
 
 
 playRound(playerSelection, computerSelection)
