9b518e6c137eff9810135c92223e87ebcb004286
aycanogut <aycanonfire@gmail.com>
2021-05-31 18:27:44 +0300

    playCount variable removed

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,5 +1,8 @@
 // DOM declares
 
+// container
+const container = document.querySelector('.nes-container');
+
 // player badges
 const badgePlayer = document.querySelector('span.is-primary');
 const badgeComputer = document.querySelector('span.is-success');
@@ -16,7 +19,6 @@ const buttons = document.querySelectorAll('.nes-btn');
 // score counter stuff
 let playerScore = 0;
 let computerScore = 0;
-let playCount = 0;
 
 // catch button value
 buttons.forEach(button => button.addEventListener('click', () => playRound(button.value)));
@@ -43,29 +45,18 @@ function disableButton() {
 
 const playRound = (playerSelection) => {
   const computerSelection = computerPlay();
-
-
-
-  if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-    playCount += 1;
-    computerScore += 1;
-    turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! AI won this round! ${playCount} round played.`;
-    badgeComputer.textContent = `${computerScore}`
-    if (computerScore === 5) {
-      turnResult.innerHTML = 'AI won! go cry';
+  if (computerPlay < 5 || playerScore < 5) {
+    if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+      computerScore++;
+      turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! AI won this round!`;
+      badgeComputer.textContent = `${computerScore}`
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors') {
+      playerScore++;
+      turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! You won this round!`;
+      badgePlayer.textContent = `${playerScore}`;
+    } else if (playerSelection === computerSelection) {
+      turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! tie!`;
     }
-  } else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors') {
-    playCount += 1;
-    playerScore += 1;
-    turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! You won this round! ${playCount} round played.`;
-    badgePlayer.textContent = `${playerScore}`;
-  } else if (playerSelection === computerSelection) {
-    playCount += 1;
-    turnResult.innerHTML = `${playerSelection} vs. ${computerSelection}! tie! ${playCount} round played.`;
-  }
-
-  if (playCounter === 5 && computerScore === 5) {
-
   }
 }
 
