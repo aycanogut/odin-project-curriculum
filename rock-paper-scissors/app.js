aee6dae01942d780125ed19bbdcd810b1d44ae9b
aycanogut <aycanonfire@gmail.com>
2021-05-31 17:34:44 +0300

    condition statement declared with less character

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -29,55 +29,35 @@ const computerPlay = () => {
   return randomNumber;
 }
 
-const computerSelection = computerPlay();
 
 const playRound = (playerSelection) => {
+  const computerSelection = computerPlay();
+  computerPlay();
 
   // score counter stuff
   let playerScore = 0;
   let computerScore = 0;
   let playCount = 0;
 
-  if (playerSelection === 'rock' && computerSelection === 'paper') {
-    turnResult.innerHTML = 'paper beats rock, AI won this round!';
+  if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
     computerScore++;
     badgeComputer.innerHTML = `${computerScore}`
+    turnResult.innerHTML = 'paper beats rock, AI won this round!';
     playCount++;
-
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+  } else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors') {
     turnResult.innerHTML = 'paper beats rock, you won this round!';
     playerScore++
     badgePlayer.innerHTML = `${playerScore}`;
     playCount++;
-
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    turnResult.innerHTML = 'scissors beats paper, you won this round!';
-    playerScore++;
-    badgePlayer.innerHTML = `${playerScore}`;
+  } else {
+    turnResult.innerHTML = 'tie!';
     playCount++;
-    return 'scissors beats paper';
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    console.log('scissors beats paper');
-    computerScore++;
-    playCount++;
-    return 'scissors beats paper';
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log('rock beats scissors');
-    playerScore++;
-    playCount++;
-    return 'rock beats scissors';
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    console.log('rock beats scissors');
-    computerScore++;
-    playCount++;
-    return 'rock beats scissors';
-  } else if (playerSelection === 'rock' && computerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'paper') {
-    console.log('tie');
-    return 'tie';
   }
 }
 
+
 playRound()
+
 
 
 // // const game = (playRound) => {
