f526170fc2e8cf9618c9e080a21a8c4d9a28f06a
aycanogut <aycanonfire@gmail.com>
2021-07-02 15:58:44 +0300

    leapYears tests are working

--- javascript-exercises-main/leapYears/leapYears.js
+++ javascript-exercises-main/leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 === 0) {
+      return true;
+    } else if (year % 100 === 0) {
+      return false;
+    }
+    return true;
+  } else return false;
 };
 
 module.exports = leapYears;
