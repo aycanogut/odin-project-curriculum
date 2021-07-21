a82d4ae42120cc0d2b18503f240fc2bf4b030897
Aycan Öğüt <aycanonfire@gmail.com>
2021-07-21 13:26:42 +0300

    container Dom and styles declared

---
+++ etch-a-sketch/js/script.js
@@ -0,0 +1,7 @@
+// DOM declarations
+const body = document.querySelector('body');
+
+const container = document.createElement('div');
+container.classList.add('container');
+
+body.appendChild(container);
