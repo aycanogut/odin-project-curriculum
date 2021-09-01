e6a9626075611b6fa4f0edd3e417adca7cd6bf33
Aycan Öğüt <aycanonfire@gmail.com>
2021-09-01 14:27:00 +0300

    added / palindrome function, all test passes

--- javascript-exercises-main/palindromes/palindromes.js
+++ javascript-exercises-main/palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = (str) => {
 
+  // regex
+  const re = /[\W_]/g;
+
+  // argument converted to lowercase and regular expression rules applied
+  let smallStr = str.toLowerCase().replace(re, '');
+
+  // smallStr variable reversed to check palidrome
+  let reverseStr = smallStr.split('').reverse().join('');
+
+  if (smallStr === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+
 
 module.exports = palindromes;
