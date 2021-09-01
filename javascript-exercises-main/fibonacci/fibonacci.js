a253c3150e56c5fd216fed2bc51532f3298a630f
Aycan Öğüt <aycanonfire@gmail.com>
2021-09-01 14:48:08 +0300

    added / fibonacci function, all test passes

--- javascript-exercises-main/fibonacci/fibonacci.js
+++ javascript-exercises-main/fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
 
-};
+  let arr = [0, 1];
+
+  if (n >= 0) {
+    for (let i = 2; i < n + 1; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr[n];
+  } else {
+    return 'OOPS';
+  }
+}
 
 module.exports = fibonacci;
