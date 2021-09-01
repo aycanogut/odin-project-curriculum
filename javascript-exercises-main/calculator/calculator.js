8e8876be2968eef2b124ec0d96dc5bd627cead98
Aycan Öğüt <aycanonfire@gmail.com>
2021-09-01 14:27:07 +0300

    added / calculator function, all test passes

--- javascript-exercises-main/calculator/calculator.js
+++ javascript-exercises-main/calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function ([...args]) {
+  return args.reduce((acc, val) => {
+    return acc += val;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function ([...args]) {
+  return args.reduce((acc, val) => {
+    return acc *= val;
+  });
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
 };
 
 module.exports = {
