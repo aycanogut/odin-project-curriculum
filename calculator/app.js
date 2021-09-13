328d6325559756a5d49dee91387ea4c005243393
aycanogut <aycanonfire@gmail.com>
2021-09-13 15:22:43 +0300

    Added math and operate functions

---
+++ calculator/app.js
@@ -0,0 +1,23 @@
+// mathematical functions
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  if (b !== 0) {
+    return a / b;
+  }
+};
+
+// operate function
+const operate = (operator) => {
+  return operator;
+};
