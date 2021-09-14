20d9db523058d0f4c647087c0d0904a0b7d0202c
aycanogut <aycanonfire@gmail.com>
2021-09-14 18:19:10 +0300

    Add calculate function

--- calculator/app.js
+++ calculator/app.js
@@ -1,4 +1,4 @@
-// mathematical functions
+//* mathematical functions
 const add = (a, b) => {
   return a + b;
 };
@@ -21,3 +21,30 @@ const divide = (a, b) => {
 const operate = (operator) => {
   return operator;
 };
+
+const calculate = () => {
+  //* DOM declarations
+  const calculator = document.querySelector(".calculator");
+  const calculatorScreen = calculator.querySelector(
+    ".calculator__screen__result"
+  );
+
+  calculator.querySelectorAll(".calculator__buttons__item").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let displayValue = event.target.innerHTML;
+
+      if (Number(displayValue) || displayValue === "." || displayValue == 0) {
+        calculatorScreen.innerHTML += displayValue;
+      } else if (displayValue === "del") {
+        displayValue.slice(0, -1);
+      } else if (displayValue === "clear") {
+        displayValue = null;
+        calculatorScreen.innerHTML = "";
+      }
+    });
+  });
+};
+
+// delete tusu fonksiyonalitesi
+// yeni numara girilince en bastaki 0'i kaldirma
+// operate fonksiyonlarini UI ile bagdastirma
