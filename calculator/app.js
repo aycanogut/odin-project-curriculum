4e4a7cefe4cd0c4ceb5fc25701ad94bb17b0f29a
aycanogut <aycanonfire@gmail.com>
2021-09-15 16:47:17 +0300

    Add more functionality to the main function

--- calculator/app.js
+++ calculator/app.js
@@ -14,6 +14,8 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   if (b !== 0) {
     return a / b;
+  } else {
+    alert(`Can't do, you simp! Divide it by another number.`);
   }
 };
 
@@ -22,29 +24,46 @@ const operate = (operator) => {
   return operator;
 };
 
+//* DOM declarations and main functionalities
 const calculate = () => {
-  //* DOM declarations
   const calculator = document.querySelector(".calculator");
-  const calculatorScreen = calculator.querySelector(
+  let calculatorScreen = calculator.querySelector(
     ".calculator__screen__result"
   );
 
+  // this array stores all the numbers which clicked
+  let arr = [];
+
+  // event listener for all butttons
   calculator.querySelectorAll(".calculator__buttons__item").forEach((item) => {
     item.addEventListener("click", (event) => {
+      // value of current number
       let displayValue = event.target.innerHTML;
 
-      if (Number(displayValue) || displayValue === "." || displayValue == 0) {
-        calculatorScreen.innerHTML += displayValue;
+      // conditional chain for 'only valid numbers'
+      if (displayValue === "." || displayValue.match("^[0-9]+$")) {
+        arr.push(displayValue);
+        calculatorScreen.innerHTML = arr.join("");
+        console.log(arr);
       } else if (displayValue === "del") {
-        displayValue.slice(0, -1);
-      } else if (displayValue === "clear") {
+        arr.pop();
+        calculatorScreen.innerHTML = arr.join("");
+        console.log(arr);
+      }
+
+      // clear the screen and other guys when 'click' pressed
+      else if (displayValue === "clear") {
         displayValue = null;
         calculatorScreen.innerHTML = "";
+        arr = [];
+        console.log(arr);
       }
     });
   });
 };
 
-// delete tusu fonksiyonalitesi
+calculate();
+
+// delete tusu fonksiyonalitesi --- OK ---
 // yeni numara girilince en bastaki 0'i kaldirma
 // operate fonksiyonlarini UI ile bagdastirma
