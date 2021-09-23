e04fbf8853ea71d764f216e74d340a70b69903ac
aycanogut <aycanonfire@gmail.com>
2021-09-23 16:50:12 +0300

    Add equal and operate functions

--- calculator/app.js
+++ calculator/app.js
@@ -30,43 +30,50 @@ keys.addEventListener("click", (e) => {
     calculator.dataset.operator = key.dataset.key; // add the operator to calculator data attr
   }
 
+  if (type === "equal") {
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+    display.textContent = operate(firstNumber, operator, secondNumber);
+  }
+
+  if (type === "clear") {
+    display.textContent = "0";
+    delete calculator.dataset.firstNumber;
+    delete calculator.dataset.operator;
+  }
+
+  if (type === "del") {
+    display.textContent = display.textContent.slice(0, -1);
+  }
+
   calculator.dataset.previosKeyType = type; // add the targets key type to the data attr
 });
 
 const operate = (firstArgument, operator, secondArgument) => {
-  switch (operator) {
-    case "+":
-      return add(firstArgument, secondArgument);
-      break;
-    case "-":
-      return subtract(firstArgument, secondArgument);
-      break;
-    case "*":
-      return multiply(firstArgument, secondArgument);
-      break;
-    case "/":
-      return divide(firstArgument, secondArgument);
-      break;
-  }
-};
+  firstArgument = Number(firstArgument);
+  secondArgument = Number(secondArgument);
 
-/* 
-this functions takes two values and returns the calculation ( simple math )
-const add = (a, b) => {
-  return a + b;
-};
-const subtract = (a, b) => {
-  return a - b;
-};
-const multiply = (a, b) => {
-  return a * b;
-};
-const divide = (a, b) => {
-  if (b === 0) {
-    console.log(b);
-    return a / b;
-  } else {
-    alert(`Can't do that, you simp! Try another number except 0.`);
+  switch (operator) {
+    case "plus":
+      return firstArgument + secondArgument;
+      break;
+    case "minus":
+      return firstArgument - secondArgument;
+
+      break;
+    case "times":
+      return firstArgument * secondArgument;
+
+      break;
+    case "divide":
+      if (secondArgument === 0) {
+        alert(
+          `Can't do that, you simp! Try to divide another number except 0.`
+        );
+      } else {
+        return firstArgument / secondArgument;
+      }
+      break;
   }
 };
-*/
